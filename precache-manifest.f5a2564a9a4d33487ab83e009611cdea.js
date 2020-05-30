f12f8bf625b9a7760a08b4eab43e7c875a4c5877
mustafa khairalla <mukhairalla@gmail.com>
2020-05-29 23:44:56 -0600

    Updates

---
+++ precache-manifest.f5a2564a9a4d33487ab83e009611cdea.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e1d365bf2cda29b813456cd3966bf6",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/index.html"
+  },
+  {
+    "revision": "d2eb0d401e17b87f63f1",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "61f43432567b55cac21b",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/css/main.4ce11c14.chunk.css"
+  },
+  {
+    "revision": "d2eb0d401e17b87f63f1",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/js/2.cf4316ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/js/2.cf4316ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f43432567b55cac21b",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/js/main.0ed51132.chunk.js"
+  },
+  {
+    "revision": "71cbdf85563ed53ff5cb",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/js/runtime-main.7ae3960d.js"
+  },
+  {
+    "revision": "d94f8b3110ae9c6abad80f20a057eb0e",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/media/bg-mustafa.d94f8b31.jpg"
+  },
+  {
+    "revision": "3d1dd3baf7d4c44e8b8c49ead7c008b7",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/media/dayplanner.3d1dd3ba.PNG"
+  },
+  {
+    "revision": "5eb39e2de3f3fdb85d477776a89dccb6",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/media/foodecipe.5eb39e2d.PNG"
+  },
+  {
+    "revision": "2098036900bef6fa34ff10a147ddaedd",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/media/mustafa.20980369.jpg"
+  },
+  {
+    "revision": "bed5a76fde84dd8951f42a295bb3773c",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/media/password.bed5a76f.PNG"
+  },
+  {
+    "revision": "fc6bcb2b9117e3aa4bece7ae4504095f",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/media/tripoints.fc6bcb2b.PNG"
+  },
+  {
+    "revision": "2c3133594c4ad20fb768ee77cb560959",
+    "url": "/MustafaKhairalla/m-khairalla-portfolio/static/media/weather.2c313359.PNG"
+  }
+]);
